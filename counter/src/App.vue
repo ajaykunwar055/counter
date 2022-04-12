286be2d062395b5905b0363e3ac173ed0c7145bb
Ajay Kunwar <ajaykunwar055@gmail.com>
2022-04-12 11:48:12 +0545

    commit

---
+++ counter/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<Counter />
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #101b25;
+  margin-top: 200px;
+}
+body {
+  background-color: rgb(253, 204, 124);
+}
+</style>
---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+   <h1> Counter </h1>
+  <h1> {{count}}</h1>
+  </div>
+  <div>
+   <button class="button button1" @click="increaseCount">Increase</button>
+   <button class="button button2" @click="decreaseCount">Decrease</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      count: 0
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+  increaseCount(){
+   this.count++
+  },
+  decreaseCount(){
+    this.count--
+  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.button {
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 10px 10px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: white;
+  color: black;
+  border: 2px solid #3ef870;
+}
+
+.button1:hover {
+  background-color: #1fee56;
+  color: white;
+}
+
+.button2 {
+  background-color: white;
+  color: black;
+  border: 2px solid #008CBA;
+}
+
+.button2:hover {
+  background-color: #008CBA;
+  color: white;
+}
+
+
+</style>
